ed926320e1dd0513be575af10fe86efd0d5a167c
Sijjad khan <sijjadkhan603@gmail.com>
2026-08-30 21:08:00 +0500

    Fix: Add boundary check in selection sort while loop

--- insertion.js
+++ insertion.js
@@ -34,7 +34,7 @@ const selectionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     let key = arr[i];
     let j = i - 1;
-    while (arr[j] > key) {
+    while (j >= 0 && arr[j] > key) {
       arr[j + 1] = arr[j];
       j--;
     }
